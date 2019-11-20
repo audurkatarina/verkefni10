870ccaeae9ffa82b89f37635c4e182474a091766
audur theodorsdottir <audurtheodorsdottir@audurs-MBP-2.lan>
2019-11-20 21:17:27 +0000

    gera localStorage virkni

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -5,6 +5,13 @@
 // Fast sem skilgreinir heiti á lykli sem vistað er undir í localStorage
 const LOCALSTORAGE_KEY = 'favourite_spacephotos';
 
+function makeList(oldList, newList) {
+  const oldString = oldList.substring(0, oldList.length - 1);
+  const newString = newList.substring(1, newList.length - 1);
+  const list = `${oldString},${newString}]`;
+  return list;
+}
+
 /**
  * Sækir gögn úr localStorage. Skilað sem lista á forminu:
  * [{ type, mediaUrl, text, title },
@@ -15,7 +22,12 @@ const LOCALSTORAGE_KEY = 'favourite_spacephotos';
  * @returns {array} fylki af myndum eða tóma fylkið ef ekkert vistað.
  */
 export function load() {
-
+  const savedImages = localStorage.getItem(LOCALSTORAGE_KEY);
+  if(savedImages){
+    const obj = JSON.parse(savedImages);
+    return obj;
+  }
+  return [];
 }
 
 /**
@@ -27,7 +39,12 @@ export function load() {
  * @param {string} title titill fyrir myndina/myndbandið.
  */
 export function save(type, mediaUrl, text, title) {
-
+  let data = [{type, mediaUrl, text, title}];
+  const savedImages = localStorage.getItem(LOCALSTORAGE_KEY);
+  if(savedImages){
+    data = JSON.parse(makeList(savedImages, JSON.stringify(data)));
+  }
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
 }
 
 
